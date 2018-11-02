f03b8ad4bf31c11a2307fb4d6ed94d0db49faca8
Ariston <thonzjavier@yahoo.com>
2018-11-02 10:12:53 +0800

    Custom Plugins
    This is for Assignment #1

---
+++ ABJPlugin/ABJPlugin/ABJPlugin.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+
+namespace ABJPlugin
+{
+    public class AppendOnAccountCreate : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            //Extract the tracing service for use in debugging sandboxed plug-ins.
+            ITracingService tracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+
+            // Obtain the execution context from the service provider.
+            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            
+            // Get a reference to the Organization service.
+            IOrganizationServiceFactory factory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+
+            IOrganizationServiceFactory servicefactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            IOrganizationService client = servicefactory.CreateOrganizationService(context.UserId);
+
+            // The InputParameters collection contains all the data passed in the message request.
+            if (context.InputParameters.Contains("Target") && context.InputParameters["Target"] is Entity)
+            {
+                // Obtain the target entity from the input parameters.
+                Entity entity = (Entity)context.InputParameters["Target"];
+
+                // Verify that the target entity represents an account.
+                // If not, this plug-in was not registered correctly.
+
+                if (entity.LogicalName == "account")
+                {
+                    try
+                    {
+                        string tempName;
+                        if (entity.Attributes.Contains("name") == true)
+                        {
+                            tempName = entity.Attributes["name"].ToString();
+                            entity.Attributes["name"] = tempName + " company";
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        tracingService.Trace("accountPlugin: {0}", ex.ToString());
+                        throw;
+                    }
+                }
+                else
+                {
+                    return;
+                }
+            }
+        }
+    }
+}
---
+++ ABJPlugin/ABJPlugin/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ABJPlugin")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ABJPlugin")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4de4160c-7c2d-4729-abc6-3243ad4b9d45")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
